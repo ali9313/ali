4c1c685ea1896515c04ceba32f4e8f0b4395fe99
ali9313 <lioali65808@gmail.com>
2024-09-26 01:42:15 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,47 @@
+# Requier Modules
+from pyrogram import Client
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+import logging, time, sys, os
+
+
+
+# Bot Configs 
+class config:
+    SESSION = "<PYROGRAM_SESSION>" # This Is Pyrogram Session Hers
+    API_KEY = "<API_KEY>" # This is Bot API Key
+    AUTH = 0000 # Sudo id
+    FORCESUB = '<CHANNLS>' # Public Channls
+    
+    API_ID = 22119881 
+    API_HASH = "95f5f60466a696e33a34f297c734d048"
+    SUDO_USERS = set() # Admins List 
+
+
+# Check Bot File Exists 
+if not os.path.exists('./.sessions'):
+    os.mkdir('./.sessions')
+
+
+bot = TelegramClient('./.sessions/rad', config.API_ID, config.API_HASH).start(bot_token=config.API_KEY) 
+userbot = Client("myacc",api_id=config.API_ID,api_hash=config.API_HASH,session_string=config.SESSION)
+
+try:
+    userbot.start()
+except BaseException:
+    sys.exit(1)
+
+
+Bot = Client(
+    "./.sessions/SaveRestricted",
+    bot_token=config.API_KEY,
+    api_id=int(config.API_ID),
+    api_hash=config.API_HASH
+)    
+
+
+try:
+    Bot.start()
+except Exception as e:
+    print(e)
+    sys.exit(1)
---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from Config import bot
+from plugins.start import *
+from plugins.progress import *
+from plugins.pyroplug import *
+from plugins.batch import *
+from plugins.frontend import *
+from plugins.helpers import *
+botStartTime = time.time()
+
+
+print(' Edit And Fix Error By Radfx2 Telegram : @R_AFX')
+
+if __name__ == "__main__":
+    bot.run_until_disconnected()
+    
