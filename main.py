19340040cacc09bdf5a907e0bd28fe2331d6f714
ali9313 <lioali65808@gmail.com>
2024-09-26 13:06:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-from Config import bot
-from plugins.start import *
+from Config import bot  # استيراد إعدادات البوت من ملف Config
+from plugins.start import *  # استيراد جميع الوحدات الإضافية
 from plugins.progress import *
 from plugins.pyroplug import *
 from plugins.batch import *
@@ -8,9 +8,7 @@ from plugins.frontend import *
 from plugins.helpers import *
 botStartTime = time.time()
 
-
 print(' Edit And Fix Error By Radfx2 Telegram : @R_AFX')
 
 if __name__ == "__main__":
-    bot.run_until_disconnected()
-    
+    bot.run()  # في Pyrogram يتم استخدام run() لبدء البوت
